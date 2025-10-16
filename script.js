c646e027c09f0b859fe40443293bfb3a98033385
Enzo Giacomini <203458225+EnzoGiacomini@users.noreply.github.com>
2025-10-15 22:43:35 -0300

    Can add task by form

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const orderList = document.getElementsByClassName('task-list')[0]; //Get ol
+const submitButton = document.getElementById('submit-button');
 
+const inputName = document.getElementById('name');
+const inputDate = document.getElementById('date');
+const inputPriority = document.getElementById('priority-form');
+const inputText = document.getElementById('description');
 
 // Add the new Task
 const addTask = (name, date, priority, text) =>
@@ -36,3 +41,20 @@ const addTask = (name, date, priority, text) =>
 
     `
 }
+
+submitButton.addEventListener('click', (event) =>{
+    event.preventDefault();
+
+    const nameValue = inputName.value;
+    const dateValue = inputDate.value;
+    const priorityValue = inputPriority.value;
+    const textValue = inputText.value;
+
+    addTask(nameValue, dateValue, priorityValue, textValue);
+
+    inputName.value = '';
+    inputDate.value = '';
+    inputPriority.value = '';
+    inputText.value = '';
+    
+});
